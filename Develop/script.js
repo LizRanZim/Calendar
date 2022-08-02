442b788fbd52b36b79596a61d120d24385914e83
LizRanZim <lrzclass22@gmail.com>
2022-08-01 22:00:12 -0400

    Set up Psuedocode

---
+++ Develop/script.js
@@ -0,0 +1,39 @@
+// use moment.js and use javascript to add events to the planer and to local storage
+// try using a for loop to check to see if the current hour is the hour when someone is looking at the calendar, if yes, turn that hour red, if the hour has passed turn the row grey, if the hour hasn't passed turn the row green (change background color)
+// use moment.js and use localstorage
+
+// use the save button on each line of the table to add an event listener to write that event to local storage and then display it
+
+
+// See this for bootstrap table setup from this site: https://getbootstrap.com/docs/4.0/content/tables/
+
+// <table class="table">
+//   <thead>
+//     <tr>
+//       <th scope="col">#</th>
+//       <th scope="col">First</th>
+//       <th scope="col">Last</th>
+//       <th scope="col">Handle</th>
+//     </tr>
+//   </thead>
+//   <tbody>
+//     <tr>
+//       <th scope="row">1</th>
+//       <td>Mark</td>
+//       <td>Otto</td>
+//       <td>@mdo</td>
+//     </tr>
+//     <tr>
+//       <th scope="row">2</th>
+//       <td>Jacob</td>
+//       <td>Thornton</td>
+//       <td>@fat</td>
+//     </tr>
+//     <tr>
+//       <th scope="row">3</th>
+//       <td>Larry</td>
+//       <td>the Bird</td>
+//       <td>@twitter</td>
+//     </tr>
+//   </tbody>
+// </table>
